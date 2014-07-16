2d491bd151c123896d640792108893a9b5690096
Jeremy Greer <jeremygreer@reergymerej.com>
2014-07-16 13:50:01 -0400

    show modules and prereqs in web

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,131 +1,166 @@
 $(function () {
-    var json;
-
-    var rand = function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    var modules = {
+      "module": [],
+      "console": [],
+      "assert": [],
+      "querystring": [],
+      "os": [
+        "module"
+      ],
+      "url": [
+        "module"
+      ],
+      "path": [
+        "module"
+      ],
+      "dns": [
+        "console"
+      ],
+      "debugger": [
+        "console"
+      ],
+      "util": [
+        "console",
+        "module"
+      ],
+      "punycode": [
+        "console",
+        "module"
+      ],
+      "buffer": [
+        "console",
+        "module"
+      ],
+      "addons": [
+        "console",
+        "module"
+      ],
+      "events": [
+        "util"
+      ],
+      "stream": [
+        "events"
+      ],
+      "timers": [
+        "module"
+      ],
+      "stringdecoder": [
+        "buffer"
+      ],
+      "dgram": [
+        "buffer",
+        "events"
+      ],
+      "vm": [
+        "util"
+      ],
+      "process": [
+        "stream"
+      ],
+      "fs": [
+        "process"
+      ],
+      "child_process": [
+        "process"
+      ],
+      "global": [
+        "process",
+        "timers"
+      ],
+      "net": [
+        "child_process"
+      ],
+      "repl": [
+        "net"
+      ],
+      "tty": [
+        "net"
+      ],
+      "tls_(ssl)": [
+        "fs",
+        "net"
+      ],
+      "readline": [
+        "tty"
+      ],
+      "crypto": [
+        "fs"
+      ],
+      "http": [
+        "net",
+        "url"
+      ],
+      "https": [
+        "fs",
+        "http",
+        "tls_(ssl)"
+      ],
+      "cluster": [
+        "http",
+        "os"
+      ],
+      "domain": [
+        "fs",
+        "http",
+        "timers"
+      ],
+      "zlib": [
+        "fs",
+        "http"
+      ]
     };
 
-    var getRandFrom = function (arr) {
-      return arr[rand(0, arr.length - 1)];
-    };
+    /*
 
-    var getRandomMethod = function () {
-      // pick module
-      var module = getRandFrom(json.modules);
-      // console.log(module.name);
+    module  
+    querystring 
+    assert  
+    console 
+    os  module
+    url module
+    path    module
+    dns console
+    debugger    console
+    timers  module
+    buffer  console,module
+    addons  console,module
+    util    console,module
+    punycode    console,module
+    stringdecoder   buffer,console,module
+    events  console,module,util
+    vm  console,module,util
+    stream  console,events,module,util
+    dgram   buffer,console,events,module,util
+    process console,events,module,stream,util
+    fs  console,events,module,process,stream,util
+    child_process   console,events,module,process,stream,util
+    global  console,events,module,process,stream,timers,util
+    net child_process,console,events,module,process,stream,util
+    crypto  console,events,fs,module,process,stream,util
+    repl    child_process,console,events,module,net,process,stream,util
+    tty child_process,console,events,module,net,process,stream,util
+    readline    child_process,console,events,module,net,process,stream,tty,util
+    tls_(ssl)   child_process,console,events,fs,module,net,process,stream,util
+    http    child_process,console,events,module,net,process,stream,url,util
+    cluster child_process,console,events,http,module,net,os,process,stream,url,util
+    zlib    child_process,console,events,fs,http,module,net,process,stream,url,util
+    https   child_process,console,events,fs,http,module,net,process,stream,tls_(ssl),url,util
+    domain  child_process,console,events,fs,http,module,net,process,stream,timers,url,util
+    */
 
-      // pick class
-      var cls = getRandFrom(module.classes);
-      // console.log(cls);
+    var ul = $('#modules');
+    var prereqDif = $('#prereqs');
 
-      // pick method
-      var method = getRandFrom(cls.methods);
+    $.each(modules, function (module) {
+        var li = $('<li>' + module + '</li>');
+        li.data('module', module);
 
-      return {
-        name: method.name,
-        textRaw: method.textRaw,
-        desc: method.desc
-      };
-    };
-
-    var right = 0,
-      wrong = 0;
-
-    var answer;
-
-    var showTally = function () {
-        $('#right').html(right);
-        $('#wrong').html(wrong);
-    };
-
-    var getMeth = function () {
-      var err;
-      var method;
-
-      do {
-        try {
-          method = getRandomMethod();
-          err = null;
-        } catch (e) {
-          err = e;
-        }
-      } while (err);
-
-      return method;
-    };
-
-    var randSortArray = function (arr) {
-      var newArr = [],
-        num;
-
-      while (arr.length) {
-        num = rand(0, arr.length - 1);
-        newArr.push(arr.splice(num, 1)[0]);
-      }
-
-      while (arr.length < newArr.length) {
-        arr.splice(arr.length - 1, 0, newArr[arr.length]);
-      }
-    };
-
-    var next = function () {
-        if (json) {
-              
-            var m1 = getMeth();
-            var methods = [];
-
-            answer = m1.name;
-
-            var i = 0;
-            for (i = 0; i < 4; i++) {
-              methods.push(getMeth());
-            }
-
-            randSortArray(methods);
-
-            var hints = '';
-            methods.forEach(function (method) {
-              hints += method.name;
-            });
-            $('#hints').html(hints);
-
-
-            
-
-            $('input[name="name"]').val('');
-            $('#textRaw, #name').hide();
-            $('#name').html(m1.name);
-            $('#textRaw').html(m1.textRaw);
-            $('#desc').html(m1.desc);
-            $('pre', '#desc').html('not shown');
-
-            showTally();
-        }
-    };
-
-    $.getJSON('http://nodejs.org/api/buffer.json', function (data) {
-        json = data;
-        next();
+        ul.append(li);
     });
 
-    $('#show').on('click', function () {
-      $('#textRaw, #name').show();
-      return false;
-    });
-
-    $('form').on('submit', function () {
-
-        var name = $('input[name="name"]').val();
-        
-        if (name === answer) {
-            right ++;
-            next();
-        } else {
-            wrong ++;
-            $('#show').click();
-            setTimeout(next, 2000);
-        }
-
-        return false;
+    ul.delegate('li', 'mouseover', function () {
+        var module = $(this).data('module');
+        var prereqs = modules[module];
+        prereqDif.html(prereqs.join(', '));
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 /* GET home page. */
 exports.index = function (req, res) {
-  res.render('index.html');
+  res.render('index', {
+    title: 'Study Node'
+  });
 };
