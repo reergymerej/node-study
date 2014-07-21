6643cb4ab0d25d1e6caf85e997ad4fea7b600c5f
Jeremy Greer <jeremygreer@reergymerej.com>
2014-07-20 20:40:51 -0400

    finish util

---
+++ modules/util.js
@@ -0,0 +1,18 @@
+// notes on util
+
+// it's not global
+var util = require('util');
+// console.log(util);
+
+var Foo = function FooConst() {};
+var Bar = function BarConst() {};
+
+Foo.prototype.a = function () {};
+Foo.prototype.b = function () {};
+Foo.prototype.c = function () {};
+
+util.inherits(Bar, Foo);
+console.log(Bar.super_.name);
+
+var b = new Bar();
+console.log(b.c);
